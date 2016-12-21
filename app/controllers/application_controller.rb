b602499f033c129f38de00a7effe728d6811bb2f
christopher floess <chris.floess@jjff.de>
2016-12-21 14:45:40 +0100

    Return random number between 1 and 100

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    msg = { :status => "ok", :message => "Success!", :html => "<b>...</b>" }
+    msg = { :status => "ok", :message => "Success!", lines: rand(1..100) }
     render json: msg
   end
 end
