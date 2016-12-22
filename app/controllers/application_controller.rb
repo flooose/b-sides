35b11962a633da58975dcad1b52acf58c4353ca2
Benjamin Rabe <benjamin@nonuts.de>
2016-12-22 16:25:01 +0100

    failure based index search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,16 @@ class ApplicationController < ActionController::Base
   def index
     circleci = Netrc.read()['circleci.com']
 
-    url = "https://circleci.com/api/v1.1/recent-builds?circle-token=#{circleci.login}&limit=20&offset=5"
+    url = "https://circleci.com/api/v1.1/recent-builds?circle-token=#{circleci.login}"
     payload = RestClient.get(url)
     tests = JSON.parse(payload.body).map do |test|
       test['status']
     end
 
-    failure = tests.index {|o| o != "success"}
+    failure = tests.index {|o| o == "failed" || o == "cancelled"}
+    if !failure
+      failure = 0
+    end
 
     msg = { :status => "ok", :message => "Success!", lines: failure }
     render json: msg
